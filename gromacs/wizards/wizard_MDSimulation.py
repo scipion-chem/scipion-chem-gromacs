203d61bbec1389aa687b2f0aca292abd2417c811
DaniDelHoyo <danieldelhoyog@gmail.com>
2022-03-24 18:20:43 +0100

    Wizard variable for listing wizards

--- gromacs/wizards/wizard_MDSimulation.py
+++ gromacs/wizards/wizard_MDSimulation.py
@@ -35,57 +35,17 @@ information such as name and number of residues.
 # Imports
 from ..protocols.protocol_MD_simulation import *
 import pyworkflow.wizard as pwizard
+from pwchem.wizards import AddElementWizard, DeleteElementWizard
 
-class GromacsAddRelaxStepWizard(pwizard.Wizard):
-    """Add a step of the workflow in the defined position"""
-    _targets = [(GromacsMDSimulation, ['insertStep'])]
+AddElementWizard().addTarget(protocol=GromacsMDSimulation,
+                             targets=['insertStep'],
+                             inputs=['insertStep'],
+                             outputs=['workFlowSteps', 'summarySteps'])
 
-    def show(self, form, *params):
-        protocol = form.protocol
-        numSteps = protocol.countSteps()
-
-        if protocol.insertStep.get().strip() != '':
-            index = int(protocol.insertStep.get())
-        else:
-            index = numSteps + 1
-        msjDic = protocol.createMSJDic()
-        if index > numSteps:
-            prevStr = protocol.workFlowSteps.get() if protocol.workFlowSteps.get() is not None else ''
-            form.setVar('workFlowSteps', prevStr + str(msjDic) + '\n')
-
-            newSum = protocol.createSummary()
-            form.setVar('summarySteps', newSum)
-
-        elif numSteps >= index > 0:
-            workSteps = protocol.workFlowSteps.get().split('\n')
-            workSteps.insert(index-1, str(msjDic))
-            form.setVar('workFlowSteps', '\n'.join(workSteps))
-
-            newSum = protocol.createSummary()
-            form.setVar('summarySteps', newSum)
-
-
-class GromacsDeleteRelaxStepWizard(pwizard.Wizard):
-    """Delete the step of the workflow defined by the index"""
-    _targets = [(GromacsMDSimulation, ['deleteStep'])]
-
-    def show(self, form, *params):
-        protocol = form.protocol
-        try:
-            index = int(protocol.deleteStep.get().strip())
-            if protocol.countSteps() >= index > 0:
-                workSteps = protocol.workFlowSteps.get().split('\n')
-                del workSteps[index - 1]
-                workSteps.remove('')
-
-                if workSteps != []:
-                    form.setVar('workFlowSteps', '\n'.join(workSteps) + '\n')
-                else:
-                    form.setVar('workFlowSteps', '')
-                newSum = protocol.createSummary()
-                form.setVar('summarySteps', newSum)
-        except:
-            print('Incorrect index')
+DeleteElementWizard().addTarget(protocol=GromacsMDSimulation,
+                                targets=['deleteStep'],
+                                inputs=['deleteStep'],
+                                outputs=['workFlowSteps', 'summarySteps'])
 
 class GromacsWatchRelaxStepWizard(pwizard.Wizard):
     """Watch the parameters of the step of the workflow defined by the index"""
