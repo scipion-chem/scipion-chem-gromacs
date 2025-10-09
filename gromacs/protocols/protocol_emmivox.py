ef791d382f9ec59b853b2d272bb615c954955395
James Krieger <jamesmkrieger@gmail.com>
2025-10-09 14:23:17 +0100

    emmivox progress

--- gromacs/protocols/protocol_emmivox.py
+++ gromacs/protocols/protocol_emmivox.py
@@ -75,13 +75,36 @@ class EMMIVoxProtocol(EMProtocol):
                       pointerClass='Volume',
                       help='This should be a main map and two half maps that the reference structure is already fitted to')
 
+        form.addParam('cutoff', params.FloatParam, label='Correlated voxel cutoff',
+                      important=True, default=0.9,
+                      help='This cutoff will be used to exclude correlated voxels (above this threshold). '
+                      'If you want to keep all the voxels of the input map, set this to 1.0')
+
+        form.addParam('zoneMap', params.BooleanParam, label='Zone volume?',
+                      important=True, default=True,
+                      help='This will zone the volume close to the reference model (optional, but speeds up things a lot)')
+
+        form.addParam('zoneDistance', params.FloatParam, label='Volume zoning distance',
+                      important=True, default=3.5,
+                      help='This distance cutoff will be used to exclude correlated voxels (above this threshold). '
+                      'If you want to keep all the voxels of the input map, set this to 1.0')
+
+        form.addParam('selection1', params.StringParam, default="protein",
+                      label="Selection string for atoms in map",
+                      help='MDAnalysis selection string for the atoms from the MD system that will be used to simulate the volume for comparison. '
+                            'This will also be used to align the reference model and volumes to the MD system')
+
+        form.addParam('selection2', params.StringParam, default="protein",
+                      label="Selection string for reference structure",
+                      help='MDAnalysis selection string for the atoms in the reference structure to align it and the volumes to the MD system')
+
     # --------------------------- STEPS functions ------------------------------
     def _insertAllSteps(self):
         # Insert processing steps
-        self._insertFunctionStep('computeStep')
+        self._insertFunctionStep('convertInputStep')
         self._insertFunctionStep('createOutputStep')
 
-    def computeStep(self):
+    def convertInputStep(self):
         inputStructFn = self.inputStructure.get().getFileName()
         if inputStructFn.endswith('.cif'):
             self.inputPdbFn = inputStructFn[:-4]+'.pdb'    
@@ -96,8 +119,9 @@ class EMMIVoxProtocol(EMProtocol):
             self.inputGroFn = inputSystem.getSystemFile()
         self.inputTopFn = inputSystem.getTopologyFile()
 
-        self.outputPdbFn = self._getPath(splitext(basename(self.inputPdbFn))[0] + '_fixed.pdb')
-        self.outputGroFn = self._getPath(splitext(basename(self.inputGroFn))[0] + '_fixed.gro')
+        # step 0, system setup, stage 1: renumber gro and pdb files using topology
+        self.outputPdbFn = self._getExtraPath(splitext(basename(self.inputPdbFn))[0] + '.pdb')
+        self.outputGroFn = self._getExtraPath(splitext(basename(self.inputGroFn))[0] + '.gro')
 
         tutorialLocation = Plugin._getEmmiVoxTutorialLocation()
         STEP1_0_LOCATION = join(tutorialLocation, '1-refinement', '0-Building')
@@ -107,6 +131,35 @@ class EMMIVoxProtocol(EMProtocol):
         self.runJob(Plugin.getEmmiVoxProgram('renumber.sh',
                                              location='tutorials/1-refinement/0-Building'),
                     args)
+
+        # step 0, system setup, stage 2: create index groups for simulating the map and writing to xtc
+        self.runJob(Plugin.getEmmiVoxProgram('make_ndx.py', python=True),
+                    '{0} {1} System-MAP --ndx index.ndx'.format(self.inputGroFn,
+                                                                self.selection1.get()))
+        self.runJob(Plugin.getEmmiVoxProgram('make_XTC_ndx.py', python=True),
+                    'System-MAP-H --ndx index.ndx')
+
+        # step 1, stage 1: map preparation
+        volumes = self.inputVolumes.get()
+        mainMap = volumes.getFileName()
+        halfMaps = volumes.getHalfMaps().split(',')
+        refPdbFn = self.inputStructure.get().getFileName()
+        selstr1 = self.selection1.get()
+        selstr2 = self.selection2.get()
+
+        args = '{0} {1} emd_plumed.dat --halfmaps {2}'.format(mainMap, self.cutoff.get(),
+                                                              ' '.join(halfMaps))
+        if self.zoneMap.get():
+            args += ' --zone {0} --zone_PDB {1} --zone_sel {2}'.format(self.zoneDistance.get(),
+                                                                      refPdbFn,
+                                                                      selstr1)
+        self.runJob(Plugin.getEmmiVoxProgram('cryo-EM_preprocess.py', python=True), args)
+
+        # step 1, stage 2: map and model alignment
+        args = '{0} {1} emd_plumed_aligned.dat emd_plumed.dat --ref_sel {2} --mobile_sel {3}'.format(self.outputPdbFn,
+                                                                                                     refPdbFn, selstr1,
+                                                                                                     selstr2)
+        self.runJob(Plugin.getEmmiVoxProgram('align-VOXELS.py', python=True), args)
 
     def createOutputStep(self):
         outputPdb = AtomStruct()
